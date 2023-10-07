f9595467b23db2865780d28d16c83bd686ce946e
Derek9745 <31524195+Derek9745@users.noreply.github.com>
2023-10-07 16:05:03 -0400

    first commit

---
+++ CalculatorConsoleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+class Calculator{
+
+public int answer;
+    
+    public static void Main(string[] args){
+        var f = new Calculator();
+        f.FunctionCheck();          
+    }
+
+    void FunctionCheck(){
+        Console.WriteLine("Please enter your first number");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter which operation you would like to use");
+        string input = Console.ReadLine()!;
+        Console.WriteLine("Please enter a second number");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+     
+        switch(input){
+                case "addition":
+                Addition(num1,num2);
+                
+                break;
+                case "subtraction":
+                Subtraction(num1, num2);
+                
+                break;
+                case "multiplicaton":
+                Multiplication(num1,num2);
+                
+                break;
+                case "division":
+                Division(num1,num2);
+                
+                break;
+        }
+        Console.WriteLine("Answer is: "+ answer);
+
+    }
+
+    public void Addition(int num1, int num2){
+            answer = num1 + num2;
+            return;
+
+    }
+    public  void Subtraction(int num1, int num2){
+            answer = num1 - num2;
+            return;
+    }
+
+    public  void Multiplication(int num1, int num2){
+             answer = num1 * num2;
+             return;
+    }
+
+    public void Division(int num1, int num2){
+            answer = num1/num2;
+            return;
+    }
+
+}
---
+++ DiceGame/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows;
+class DiceGame{
+
+
+ public static void Main(String[] Args){
+   
+  Console.WriteLine("Welcome to The Last Throw \nwhich mode would you like to play?\n1. Best of 1\n2. Best of 3\n3. Best of 5");
+  int answer = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine(answer);
+  startGame(answer);
+}
+
+public  static int startGame(int answer){
+
+  switch(answer){
+      case 1:
+        Console.WriteLine("Beginning best of 1");
+        int playerResult = Rolldice();
+        int computerResult = Rolldice();
+        Console.WriteLine("You rolled a" + playerResult + " Your opponent rolled a " + computerResult);
+
+          if(playerResult>computerResult){
+            Console.WriteLine("You Win!");
+          }
+          else if(playerResult == computerResult){
+            Console.WriteLine("Draw!");
+          }
+          else if (playerResult <computerResult){
+            Console.WriteLine("You Lose");
+        }
+        break;
+
+      case 2: 
+        Console.WriteLine("Beginning best of 3");
+        break;
+
+      case 3: 
+        Console.WriteLine("Beginning best of 5");
+        break;
+    }
+
+    return answer;
+
+}
+
+  static int Rolldice(){
+
+      Random rand = new Random();
+      int numb = rand.Next(1,6);
+      return numb;
+
+  }
+
+
+
+}
+
+ 
